361b499b04a6171edea40c8b3891716f2a8d75c9
Crowdbotics <team@crowdbotics.com>
2020-11-11 21:57:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wolf_tech_go_22539/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wolf_tech_go_22539;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "wolf_tech_go_22539";
+    }
+}
